14a5e8d95b5ddff272197c1c9b191522346d6e5c
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-11 14:35:22 +0100

    wishcontroller

---
+++ src/Controller/WishController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\WishRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/wishes', name: 'wish_')]
+final class WishController extends AbstractController
+{
+    #[Route('', name: 'list')]
+    public function list(WishRepository $wishRepository): Response
+    {
+        $wishes = $wishRepository->findAll();
+
+        return $this->render('wish/list.html.twig', [
+            'wishes' => $wishes
+        ]);
+    }
+
+    #[Route('/{id}', name: 'detail', requirements: ['id' => '[0-9]+'])]
+    public function detail(int $id): Response
+    {
+        return $this->render('wish/detail.html.twig', [
+            'wishId' => $id
+        ]);
+    }
+}
