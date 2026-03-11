4bc03e16ecde4aeb8e77107e1f05e659ff0a9ac7
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-11 16:01:49 +0100

    récupère par id parce que l'autre marche pas

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -21,11 +21,12 @@ final class WishController extends AbstractController
     }
 
     #[Route('/{id}', name: 'detail', requirements: ['id' => '[0-9]+'])]
-    public function detail(Wish $id, WishRepository $wishRepository): Response
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
+        $wish = $wishRepository->find($id);
 
         return $this->render('wish/detail.html.twig', [
-            'wish' => $id
+            'wish' => $wish
         ]);
     }
 }
