56965f96b51061ae3445d0bab669cd4fb911ca33
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-12 16:26:40 +0100

    Show the flash messages

--- src/Entity/Wish.php
+++ src/Entity/Wish.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\WishRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: WishRepository::class)]
 class Wish
@@ -15,12 +16,14 @@ class Wish
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank()]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $description = null;
 
     #[ORM\Column(length: 64)]
+    #[Assert\NotBlank()]
     private ?string $author = null;
 
     #[ORM\Column]
