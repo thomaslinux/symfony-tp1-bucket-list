0fcb715189ea7d45c33d8c6c9c23b6317ac7431c
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-17 14:09:39 +0100

    ajout des categories à la bucket list

---
+++ src/Form/WishType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Wish;
+use App\Repository\CategoryRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WishType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Your idea'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Please describe it'
+            ])
+            ->add('author', TextType::class, [
+                'label' => 'Your username'
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'query_builder' => function (CategoryRepository $categoryRepository) {
+                    return $categoryRepository->createQueryBuilder('c')->addOrderBy('c.name');
+                }
+
+            ])
+            ->add('validate', SubmitType::class, [
+                'label' => 'Save'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Wish::class,
+            'required' => false
+        ]);
+    }
+}
