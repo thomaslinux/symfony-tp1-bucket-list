59766d170ade776585fabf600c22e4c95b801cbf
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-19 11:26:53 +0100

    méthode alternative via Symfony, plus adaptée à du PUT et PATCH

--- src/Services/EventService.php
+++ src/Services/EventService.php
@@ -2,19 +2,20 @@
 
 namespace App\Services;
 
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class EventService
 {
     private readonly string $BASE_URL;
 
-    public function __construct()
+    public function __construct(private HttpClientInterface $httpClient)
     {
-        $this->BASE_URL = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=evenements-publics-openagenda";
+        $this->BASE_URL = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=evenements-publics-openagenda&refine.firstdate_begin=2026-03-01";
     }
 
     public function getDataFromAPI()
     {
-        $json = file_get_contents($this->BASE_URL);
-        $data = json_decode($json, true);
+        $data = $this->httpClient->request('GET', $this->BASE_URL)->toArray();
 
         return $data['records'];
     }
