29f702e57128073a1ab0cd6f1c9fe9473d35bfdb
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-19 11:10:01 +0100

    ma:con Event envoyer les données récupérées

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 final class MainController extends AbstractController
 {
