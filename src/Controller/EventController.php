874f02bcce97187bfe0670e5b6b6f3053cb60371
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-19 11:46:07 +0100

    extraire les données du formulaire et les ajouter dans la requête

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -6,17 +6,19 @@ use App\Form\EventSearchType;
 use App\Form\Model\EventSearch;
 use App\Services\EventService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class EventController extends AbstractController
 {
     #[Route('/events', name: 'event_list')]
-    public function list(EventService $eventService): Response
+    public function list(EventService $eventService, Request $request): Response
     {
 
         $eventSearch = new EventSearch();
         $eventForm = $this->createForm(EventSearchType::class, $eventSearch);
+        $eventForm->handleRequest($request);
 
         $events = $eventService->getDataFromAPI();
 
