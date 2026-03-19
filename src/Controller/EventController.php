b17bcc0502c86ed6d615431b7827b538f8cb74bf
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-19 11:10:07 +0100

    ma:con Event envoyer les données récupérées

---
+++ src/Controller/EventController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class EventController extends AbstractController
+{
+    #[Route('/events', name: 'event_list')]
+    public function list(): Response
+    {
+
+        $json = file_get_contents("https://public.opendatasoft.com/api/records/1.0/search/?dataset=evenements-publics-openagenda");
+        $data = json_decode($json);
+        dd($data);
+
+        return $this->render('event/list.html.twig', [
+            'events' => $data['records']
+        ]);
+    }
+}
