5d42d86ec5ec8c8df1fcc6f8b81a3b45a21cd511
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-27 14:51:41 +0100

    ajouter une recherche par paramètres

--- src/Repository/WishRepository.php
+++ src/Repository/WishRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Wish;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use function Doctrine\ORM\QueryBuilder;
 
 /**
  * @extends ServiceEntityRepository<Wish>
@@ -42,6 +43,31 @@ class WishRepository extends ServiceEntityRepository
             ->addSelect('c')
             ->andWhere('w.isPublished = :ispublished')
             ->setParameter('ispublished', true)
+            ->addOrderBy('w.dateCreated', 'DESC');
+
+        $query = $qb->getQuery();
+        return $query->getResult();
+    }
+
+    public function findWishesByTitle($wishTitle, $wishCategory)
+    {
+        $qb = $this->createQueryBuilder('w');
+        $qb
+            ->join('w.category', 'c')
+            ->addSelect('c')
+            ->andWhere('w.isPublished = :ispublished')
+            ->setParameter('ispublished', true);
+        if ($wishTitle) {
+            $qb
+                ->andWhere($qb->expr()->like('w.title', ':wishTitle'))
+                ->setParameter('wishTitle', '%' . $wishTitle . '%');
+        }
+        if ($wishCategory) {
+            $qb
+                ->andWhere('w.category = :categoryId')
+                ->setParameter('categoryId', $wishCategory);
+        }
+        $qb
             ->addOrderBy('w.dateCreated', 'DESC');
 
         $query = $qb->getQuery();
